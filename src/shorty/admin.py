d320863239f7e7e847411697d6bf69862d7675a0
Matt George <matt.george@myemma.com>
2009-04-30 17:57:09 -0500

    removed shorty app

--- src/shorty/admin.py
+++
@@ -1,5 +0,0 @@
-from models import ShortUrl, ClickEntry
-from django.contrib import admin
-
-admin.site.register(ShortUrl)
-admin.site.register(ClickEntry)
--- src/shorty/forms.py
+++
@@ -1,6 +0,0 @@
-from django.forms import ModelForm
-from shorty.models import ShortUrl
-class UrlForm(ModelForm):
-    class Meta:
-        model = ShortUrl
-    
